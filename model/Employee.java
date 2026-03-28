665fc59a8317641cd6d8b3d4dc37fc0f98068eec
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-03-28 18:03:38 -0500

    Agregué la clase de Employee al código

--- model/Employee.java
+++ model/Employee.java
@@ -1,5 +1,56 @@
 package BankProject.model;
+import java.util.Date;
 
-public class Employee {
+public class Employee extends BaseClass {
+    private String name;
+    private String position;
+    private double salary;
+    private Date hireDate;
+    private String speciality;
+    private static int autoId;
+    public Employee(String name, String position, double salary, Date hireDate, String speciality) {
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+        this.hireDate = hireDate;
+        this.speciality = speciality;
+        this.autoId = ++autoId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public Date getHireDate() {
+        return hireDate;
+    }
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+    public String getSpeciality() {
+        return speciality;
+    }
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", position=" + position + ", salary=" + salary + ", hireDate=" + hireDate
+                + ", speciality=" + speciality + "]";
+    }
+    
     
 }
