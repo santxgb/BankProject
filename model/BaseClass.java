3476a04b1b8f9d580cd4e72284f7e172aaaf16a4
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-03-28 17:46:44 -0500

    Creé las clases BaseClass y Customer con la herencia integrada y además el atributo autoId para el tema de la cantidad de objetos que se crean

--- model/BaseClass.java
+++ model/BaseClass.java
@@ -1,5 +1,23 @@
 package BankProject.model;
 
 public class BaseClass {
+    protected int id;
+
+    public BaseClass() {
+        super();
+    }
+
+    public BaseClass(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     
 }
--- model/Customer.java
+++ model/Customer.java
@@ -1,5 +1,58 @@
 package BankProject.model;
+import java.util.Date;
 
-public class Customer {
+public class Customer extends BaseClass {
+    private String name;
+    private String idNumber;
+    private String email;
+    private String phone;
+    private Date registrationDate;
+    private static int autoId;
+    public Customer() {
+    }
+    public Customer(String name, String idNumber, String email, String phone, Date registrationDate) {
+        this.name = name;
+        this.idNumber = idNumber;
+        this.email = email;
+        this.phone = phone;
+        this.registrationDate = registrationDate;
+        this.id = ++autoId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getIdNumber() {
+        return idNumber;
+    }
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+    @Override
+    public String toString() {
+        return "Customer [name=" + name + ", idNumber=" + idNumber + ", email=" + email + ", phone=" + phone
+                + ", registrationDate=" + registrationDate + "]";
+    }
+    
     
 }
