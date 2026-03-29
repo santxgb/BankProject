da507fcddd0fd9a98a4df89a8f77fda0b7b286f7
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-03-29 12:39:32 -0500

    Agregué las clases de Bank y Commercial Bank

--- model/Bank.java
+++ model/Bank.java
@@ -1,5 +1,67 @@
 package BankProject.model;
 
-public class Bank {
-    
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Bank extends BaseClass {
+    protected String name;
+    protected String nit;
+    protected String address;
+    protected String phone;
+    protected Date foundationDate;
+    protected double totalAssets;
+    protected List<Customer> customerList;
+    protected List<Employee> employeeList;
+    protected List<Account> accountList;
+    private static int autoId;
+
+    public Bank(String name, String nit, String address, String phone,
+                Date foundationDate, double totalAssets) {
+        this.name = name;
+        this.nit = nit;
+        this.address = address;
+        this.phone = phone;
+        this.foundationDate = foundationDate;
+        this.totalAssets = totalAssets;
+        this.customerList = new ArrayList<>();
+        this.employeeList = new ArrayList<>();
+        this.accountList = new ArrayList<>();
+        this.id = ++autoId;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getNit() { return nit; }
+    public void setNit(String nit) { this.nit = nit; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public Date getFoundationDate() { return foundationDate; }
+    public void setFoundationDate(Date foundationDate) { this.foundationDate = foundationDate; }
+
+    public double getTotalAssets() { return totalAssets; }
+    public void setTotalAssets(double totalAssets) { this.totalAssets = totalAssets; }
+
+    public List<Customer> getCustomerList() { return customerList; }
+    public void setCustomerList(List<Customer> customerList) { this.customerList = customerList; }
+
+    public List<Employee> getEmployeeList() { return employeeList; }
+    public void setEmployeeList(List<Employee> employeeList) { this.employeeList = employeeList; }
+
+    public List<Account> getAccountList() { return accountList; }
+    public void setAccountList(List<Account> accountList) { this.accountList = accountList; }
+
+    @Override
+    public String toString() {
+        return "Bank [id=" + id + ", name=" + name + ", nit=" + nit +
+               ", address=" + address + ", phone=" + phone +
+               ", foundationDate=" + foundationDate + 
+               ", totalAssets=" + totalAssets + "]";
+    }
 }
--- model/CommercialBank.java
+++ model/CommercialBank.java
@@ -1,5 +1,48 @@
 package BankProject.model;
 
-public class CommercialBank {
-    
+import java.util.Date;
+
+public class CommercialBank extends Bank {
+    private double loanInterestRate;
+    private double creditLimit;
+    private int atmCount;
+    private boolean offersMortgage;
+    private String bankCategory;
+
+    public CommercialBank(String name, String nit, String address, String phone,
+                          Date foundationDate, double totalAssets,
+                          double loanInterestRate, double creditLimit,
+                          int atmCount, boolean offersMortgage, String bankCategory) {
+        super(name, nit, address, phone, foundationDate, totalAssets);
+        this.loanInterestRate = loanInterestRate;
+        this.creditLimit = creditLimit;
+        this.atmCount = atmCount;
+        this.offersMortgage = offersMortgage;
+        this.bankCategory = bankCategory;
+    }
+
+    public double getLoanInterestRate() { return loanInterestRate; }
+    public void setLoanInterestRate(double loanInterestRate) { this.loanInterestRate = loanInterestRate; }
+
+    public double getCreditLimit() { return creditLimit; }
+    public void setCreditLimit(double creditLimit) { this.creditLimit = creditLimit; }
+
+    public int getAtmCount() { return atmCount; }
+    public void setAtmCount(int atmCount) { this.atmCount = atmCount; }
+
+    public boolean isOffersMortgage() { return offersMortgage; }
+    public void setOffersMortgage(boolean offersMortgage) { this.offersMortgage = offersMortgage; }
+
+    public String getBankCategory() { return bankCategory; }
+    public void setBankCategory(String bankCategory) { this.bankCategory = bankCategory; }
+
+    @Override
+    public String toString() {
+        return "CommercialBank [id=" + id + ", name=" + name + ", nit=" + nit +
+               ", address=" + address + ", phone=" + phone +
+               ", foundationDate=" + foundationDate + ", totalAssets=" + totalAssets +
+               ", loanInterestRate=" + loanInterestRate + ", creditLimit=" + creditLimit +
+               ", atmCount=" + atmCount + ", offersMortgage=" + offersMortgage +
+               ", bankCategory=" + bankCategory + "]";
+    }
 }
